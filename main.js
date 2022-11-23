01623fe6e0200969c527c8d121b9fad5b95e94ff
arsil <arsilq@yahoo.com>
2022-11-23 17:09:02 +0700

    Added backdrop, Enhance transition

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-$(document).ready(function(){
-  $('#book-carousel').slick({
+$(document).ready(function () {
+  // $("#sidebar").hide();
+  $("#book-carousel").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
-    initialSlide:1,
+    initialSlide: 1,
     centerMode: true,
-    variableWidth:true,
+    variableWidth: true,
     arrows: true,
     dots: false,
     speed: 300,
-    centerPadding: '0px',
+    centerPadding: "0px",
     infinite: true,
     autoplaySpeed: 5000,
-    autoplay: true
+    autoplay: true,
   });
+
 });
+function isMobileWidth() {
+  return $('#mobile-indicator').is(':visible');
+}
+console.log(isMobileWidth())
