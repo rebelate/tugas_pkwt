056c11fee208b3cb3c18a19260c1b58c457a9a0f
arsil <arsilq@yahoo.com>
2022-11-24 11:33:26 +0700

    Added transition

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   $("#book-carousel").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
-    initialSlide: 1,
+    initialSlide: 0,
     centerMode: true,
     variableWidth: true,
     arrows: true,
@@ -11,8 +11,10 @@ $(document).ready(function () {
     speed: 300,
     centerPadding: "0px",
     infinite: true,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 500,
     autoplay: true,
   });
-
+  setTimeout(() => {
+    $("#book-carousel").slick("slickSetOption", "autoplaySpeed", 3000, true); // Set autoplaySpeed to 8 seconds.
+  }, 501);
 });
